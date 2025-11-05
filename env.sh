c7558d44b2696b81f55921f2ca08d074ee35d0ea
Ping Luo <luop0812@gmail.com>
2025-11-05 18:55:38 -0500

    test identity file

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 module load miniconda
 conda create -p $(pwd)/conda_env python=3.10 jupyterlab notebook bash_kernel nvidia/label/cuda-12.8.1::cuda-toolkit  numpy scipy matplotlib pandas plotly
 conda activate /gpfs/radev/home/pl543/project/git-sources/training/misha-2025-fall/conda_env
-pip3 install torch torchvision
+
+pip install torch torchvision
